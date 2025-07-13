04cac02b23e1251a9667131e56ab3cc875786402
Rajkumar Yogi <rajkumar6777y@gmail.com>
2025-07-13 15:57:36 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const adminRoutes = require('./routes/adminRoutes');
 
 // Enhanced CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:5173','https://e-commerce-theta-coral-19.vercel.app/'],
+  origin: ['http://localhost:5173','https://e-commerce-theta-coral-19.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
